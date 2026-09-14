4d61e545d32fe27a808fdf4d7fbc898db5043acd
Bittori Alfaro <bittori.alfaro@fms-ea.com>
2026-09-14 13:00:22 +0200

    Ajout structure minimal du projet Pycharm

---
+++ goncourt/models/person.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+"""
+Classe abstraite Person, mère de Author et Jury_member
+"""
+
+from abc import ABC
+from dataclasses import dataclass, field
+
+
+@dataclass
+class Person(ABC):
+    """Personne liée au concours : auteur ou membre du jury."""
+    first_name: str
+    last_name: str
+
+    def __str__(self) -> str:
+        return f"{self.first_name} {self.last_name} ({self.age} ans)"
